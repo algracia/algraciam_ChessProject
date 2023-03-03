c93ccd2d4cf498a07537048dd6cec82bdbc3d47e
algracia <agraciamartelo@gmail.com>
2023-03-03 05:04:41 -0500

    basic_config_again

---
+++ Basic_config/Src/main.c
@@ -0,0 +1,19 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : Alberto Gracia Martelo
+ * @brief          : Main program body
+ ******************************************************************************
+*COnfiguracion base del ambiente de desarrollo
+ ******************************************************************************
+ */
+
+#include <stdint.h>
+
+int main(void) {
+
+	/* Loop forever */
+	while (1) {
+
+	}
+}
