5cab6b54cac1c35f99d33813d4e54e17a22e1298
algracia <agraciamartelo@gmail.com>
2023-04-09 17:28:00 -0500

    Depuracion del codigo de la Tarea3

--- SolucionTarea3/Src/mainSolucionTarea3.c
+++ SolucionTarea3/Src/mainSolucionTarea3.c
@@ -4,7 +4,7 @@
  * @author         : Alberto Gracia Martelo
  * @brief          : Main program body
  ******************************************************************************
-*COnfiguracion base del ambiente de desarrollo
+*Desarrollo de la Tarea 3
  ******************************************************************************
  */
 
@@ -45,58 +45,70 @@ EXTI_Config_t handlerEXTISW					={0};
 
 /*Flags*/
 //Estas banderas iran dentro de los callback que correpondan
-//y permitiran ejecutar en el main el codigo relacionado
+//y permitiran ejecutar en la funcion main el codigo relacionado
 //a su interrupcion correspondiente
 
 uint8_t Exti4Flag	=0;
 uint8_t Timer3Flag 	=0;
-uint8_t ModeFlag	=0;
+uint8_t ModeFlag	=0;  //Indica si se esta en modo "Conteo" o "Culebrita"
 
 
 /*Variables*/
 uint8_t data 				=0;
 uint8_t clk					=0;
+uint8_t displaySelect		=0;
 int8_t unitCounter  		=0;
 int8_t decimalCounter  		=0;
 int8_t culebritaCounter 	=0;
 
+
 /*Funciones*/
-void initHardware (void);
+void InitHardware (void);
 void Numero7Segmentos (uint8_t numero);
 void CulebritaConfig (uint8_t pos);
 uint8_t DisplaySelect (int8_t pos_disp);
 
-uint8_t var1 =0;
+
 
 int main(void) {
 
 	/*Cargamos toda la configuracion del hardware*/
-	initHardware ();
+	InitHardware ();
 
 
 	/* Loop forever*/
 	while (1) {
 
+		//Se genera un switch-case que discrimine ambos modos
 		switch (ModeFlag){
 
 		case 0:{
+			/*Este caso corresponde al modo conteo*/
 
 			if(Timer3Flag){
+				//Se intercambian los valores de los pines
+				//que controlan a los transistores, esto
+				//genera el switcheo entre los dos displays
 				GPIOxTooglePin(&handlerPuertoC11);
 				GPIOxTooglePin(&handlerPuertoC10);
 
 				/*Configuramos como se van a mostrar los numeros en el 7 segmentos*/
+
 				if(GPIO_ReadPin(&handlerPuertoC11)){
+					//Si el puerto C11(el de las unidades) esta con un 1 logico
+					//entonces el display de las unidades esta apagdo y el
+					//de las decimas esta encendido, con lo cual, se configura
 					Numero7Segmentos(decimalCounter);
-					//Si el puerto C11(el de las decimas) esta apagado (con 1 logico)
-					//el de las unidades es el que esta encendido y se configura
 
 				}else{
+					//Por otro lado, si el puerto C11(el de las unidades)
+					//esta con un 0 logico entonces el display de las
+					//unidades encendido, con lo cual, este se configura
 					Numero7Segmentos(unitCounter);
-					//Si el puerto C11(el de las decimas) esta encendido (con 0 logico)
-					//se confura ese mismo
+
 				}
-			Timer3Flag =0;
+			Timer3Flag =0; //Se baja la bandera del Timer 3
+
 			}//Fin del 'if' del Timer3
 
 
@@ -106,33 +118,47 @@ int main(void) {
 			if(Exti4Flag){
 
 				if (data > clk){
+					//Sentido CW
 					unitCounter++;
 
 					if(unitCounter > 9){
 						unitCounter =0;
 						decimalCounter++;
+						//Cada que las unidades pasen de 9,
+						//la decimas aumentan en 1 y las unidades vuelven a 0
 
 						if (decimalCounter > 9){
 							unitCounter 	=9;
 							decimalCounter 	=9;
+
+							//Ademas, si tanto unidadades como decimas pasan de 9,
+							//su valor se mantendra estatico e igual a 9,
+							//de este modo, los displays no muestran un numero mayor a 99
 						}
 					}
 
 
 				}else{
+					//Sentido CCW
 					unitCounter--;
 
 					if(unitCounter < 0){
 						unitCounter =9;
 						decimalCounter--;
+						//Cada que las unidades bajen de 0,
+						//la decimas disminuyen en 1 y las unidades vuelven a 9
 
 						if (decimalCounter < 0){
 							unitCounter 	=0;
 							decimalCounter  =0;
+							//Ademas, si tanto unidadades como decimas bajan de 0,
+							//su valor se mantendra estatico e igual a 0,
+							//de este modo, los displays no muestran un numero menor a 00
 						}
 					}
 				}
-				Exti4Flag =0;
+				Exti4Flag =0;// Se baja el Flag del EXTI4
+
 			}//Fin del 'if' del Exti del encoder
 
 			break;
@@ -140,70 +166,81 @@ int main(void) {
 
 
 		case 1:{
+			/*Este caso corresponde al modo "Culebrita"*/
 
 			if(Timer3Flag){
 				GPIOxTooglePin(&handlerPuertoC10);
 				GPIOxTooglePin(&handlerPuertoC11);
 
-				var1 = DisplaySelect(culebritaCounter);
+				//Revisamos en que display se deberia estar visualizando el segmento
+				displaySelect = DisplaySelect(culebritaCounter);
 
-//				switch (DisplaySelect(culebritaCounter)){
-//				case DISPUNI:{
-				if(var1 ==0){
+				if(displaySelect ==DISPUNI){
+
+					//Si se debe visualizar en las unidades, entonces apagamos el display de las decenas
+					//y dejamos encendido el de las unidades con su respectiva configuracion.
 
 					if(GPIO_ReadPin(&handlerPuertoC11)){
-						//Si el puerto C11(el de las unidades) esta apagado (con 1 logico)
-						//el de las decimas es el que esta encendido y se configura
+						//Si el puerto C11(el de las unidades) esta con un 1 logico
+						//entonces el display de las unidades esta apagdo y el
+						//de las decimas esta encendido, con lo cual, se configura
 						CulebritaConfig(ALL_OFF);
 
 					}else{
+						//Por otro lado, si el puerto C11(el de las unidades)
+						//esta con un 0 logico entonces el display de las
+						//unidades encendido, con lo cual, este se configura
 						CulebritaConfig(culebritaCounter);
 					}
 
 				}else{
+
+					//Si se debe visualizar en las decenas, entonces apagamos el display de las unidades
+					//y dejamos encendido el de las decenas con su respectiva configuracion.
+
 					if(GPIO_ReadPin(&handlerPuertoC11)){
-						//Si el puerto C11(el de las unidades) esta apagado (con 1 logico)
-						//el de las decimas es el que esta encendido y se configura
+						//Si el puerto C11(el de las unidades) esta con un 1 logico
+						//entonces el display de las unidades esta apagdo y el
+						//de las decimas esta encendido, con lo cual, se configura
 						CulebritaConfig(culebritaCounter);
 
 					}else{
+						//Por otro lado, si el puerto C11(el de las unidades)
+						//esta con un 0 logico entonces el display de las
+						//unidades encendido, con lo cual, este se configura
 						CulebritaConfig(ALL_OFF);
 					}
 				}
 
-//				case DISPDEC:{
-
-//					if(GPIO_ReadPin(&handlerPuertoC11)){
-//						//Si el puerto C11(el de las unidades) esta apagado (con 1 logico)
-//						//el de las decimas es el que esta encendido y se configura
-//						CulebritaConfig(culebritaCounter);
-//
-//					}else{
-//						CulebritaConfig(ALL_OFF);
-//					}
-//				//}
-//				//}
-				Timer3Flag =0;
+				Timer3Flag =0; // Se baja la bandela del TIMER3
 			}
 
 			if(Exti4Flag){
 
 				if (data > clk){
+
+					//Sentido CW
 					culebritaCounter++;
 
 						if (culebritaCounter > 11){
+							//Cada que se supera la posicion final,
+							//la culebrita se reinicia y vuelve a su posicion incial
 							culebritaCounter 	=0;
 						}
 					}
 
 				else{
+
+					//Sentido CCW
 					culebritaCounter--;
 
 						if (culebritaCounter < 0){
 							culebritaCounter =11;
+							//Cada que se llega mas abajo que la posicion inicial,
+							//la culebrita vuelve a empezar desde su posicion final
 						}
 					}
-				Exti4Flag =0;
+				Exti4Flag =0; //Se baja la bandera del EXTI4
 				}
 
 				break;
@@ -218,7 +255,7 @@ int main(void) {
 }
 
 
-void initHardware (void){
+void InitHardware (void){
 
 		/*Configuracion del Blinky*/
 		handlerOnBoardLed.pGPIOx 								= GPIOA;
@@ -261,6 +298,7 @@ void initHardware (void){
 		ExtInt_Config(&handlerEXTIEncoder);
 
 
+
 		/*Configuracion del boton del encoder*/
 		handlerPuertoA0.pGPIOx								= GPIOA;
 		handlerPuertoA0.GPIO_PinConfig.GPIO_PinNumber		= PIN_0;
@@ -276,6 +314,7 @@ void initHardware (void){
 		ExtInt_Config(&handlerEXTISW);
 
 
+
 		/*Configuracion del 7 segmentos*/
 
 		//Configuracion del swicheo de los 7 segmentos
@@ -287,7 +326,7 @@ void initHardware (void){
 		handlerPuertoC10.GPIO_PinConfig.GPIO_PinSpeed			= GPIO_OSPEED_FAST;
 
 		GPIO_Config(&handlerPuertoC10);
-		GPIO_WritePin(&handlerPuertoC10, 0); //Hacemos que este pin sea un cero logico a la salida (Switchea unidades)
+		GPIO_WritePin(&handlerPuertoC10, 0); //Hacemos que este pin sea un cero logico a la salida (Switchea decenas)
 
 		handlerPuertoC11.pGPIOx 								= GPIOC;
 		handlerPuertoC11.GPIO_PinConfig.GPIO_PinNumber			= PIN_11;
@@ -297,7 +336,7 @@ void initHardware (void){
 		handlerPuertoC11.GPIO_PinConfig.GPIO_PinSpeed			= GPIO_OSPEED_FAST;
 
 		GPIO_Config(&handlerPuertoC11);
-		GPIO_WritePin(&handlerPuertoC11, 1); //Hacemos que este pin sea un 1 logico a la salida (Switchea decenas)
+		GPIO_WritePin(&handlerPuertoC11, 1); //Hacemos que este pin sea un 1 logico a la salida (Switchea unidades)
 
 		//De este modo garantizamos que las salidas de ambos pines seran opuestas
 		//y asi, cuando uno este encendido, el otro estará apagado
@@ -322,7 +361,7 @@ void initHardware (void){
 		 * F -> PB0
 		 * G -> PC0
 		 *
-		 * Anodos: PC10 y PC11
+		 * Ánodos: PC10 y PC11
 		 */
 		handlerPuertoC12.pGPIOx 								= GPIOC;
 		handlerPuertoC12.GPIO_PinConfig.GPIO_PinNumber			= PIN_12;
@@ -332,7 +371,7 @@ void initHardware (void){
 		handlerPuertoC12.GPIO_PinConfig.GPIO_PinSpeed			= GPIO_OSPEED_FAST;
 
 		GPIO_Config(&handlerPuertoC12);
-		GPIO_WritePin(&handlerPuertoC12, 0);
+		GPIO_WritePin(&handlerPuertoC12, 0); //Enviamos todos los pines a un valor conocido
 
 		handlerPuertoA15.pGPIOx 								= GPIOA;
 		handlerPuertoA15.GPIO_PinConfig.GPIO_PinNumber			= PIN_15;
@@ -362,7 +401,7 @@ void initHardware (void){
 		handlerPuertoC1.GPIO_PinConfig.GPIO_PinSpeed			= GPIO_OSPEED_FAST;
 
 		GPIO_Config(&handlerPuertoC1);
-		GPIO_WritePin(&handlerPuertoC1, 1);
+		GPIO_WritePin(&handlerPuertoC1, 0);
 
 		handlerPuertoC2.pGPIOx 									= GPIOC;
 		handlerPuertoC2.GPIO_PinConfig.GPIO_PinNumber			= PIN_2;
@@ -392,9 +431,10 @@ void initHardware (void){
 		handlerPuertoB0.GPIO_PinConfig.GPIO_PinSpeed			= GPIO_OSPEED_FAST;
 
 		GPIO_Config(&handlerPuertoB0);
-		GPIO_WritePin(&handlerPuertoB0, 1);
+		GPIO_WritePin(&handlerPuertoB0, 0);
 
 }//Termina el initHardware
+
 
 /*Funcion para configurar los numeros que se van a visualizar*/
 void Numero7Segmentos (uint8_t numero){
@@ -519,6 +559,10 @@ void Numero7Segmentos (uint8_t numero){
 	}
 }//Fin de la funcion Numero7Segmentos
 
+
+/* Funcion para configurar la visualizacion
+ * del segmento en cada posicion del modo "Culebrita"
+ */
 void CulebritaConfig (uint8_t pos){
 
 	switch (pos){
@@ -668,6 +712,10 @@ void CulebritaConfig (uint8_t pos){
 	}
 
 	case ALL_OFF:{
+		//Este caso es para cuando el segmento no este
+		//en uno de los displays, este display se encuentre
+		//totalmente apagado.
+
 		GPIO_WritePin(&handlerPuertoC12,1); //A
 		GPIO_WritePin(&handlerPuertoA15,1);	//B
 		GPIO_WritePin(&handlerPuertoC2, 1);	//C
@@ -683,8 +731,12 @@ void CulebritaConfig (uint8_t pos){
 		break;
 	}
 	}
-}
+}//Fin de la funcion CulebritaConfig
 
+
+/*Funcion para indicar que display esta activo
+ * en cada posicion del modo "Culebrita"
+ */
 uint8_t DisplaySelect (int8_t pos_disp){
 
 	uint8_t display =0;
@@ -705,7 +757,9 @@ uint8_t DisplaySelect (int8_t pos_disp){
 	default:{break;}
 	}
 	return display;
-}
+
+}//Fin de la funcion DisplaySelect
+
 
 /*Callbacks de los timers*/
 void BasicTimer2_Callback(void){
@@ -721,15 +775,15 @@ void BasicTimer3_Callback(void){
 /*Callbacks de los EXTI*/
 void callback_extInt0(void){
 	//Interrupcion que controla el "Modo" del 7 segmentos,
-	//ya sea contador o culebrita (asociado al boton SW del encoder)
+	//ya sea conteo o culebrita (asociado al boton SW del encoder)
 	ModeFlag ^=1;
 }
 
 void callback_extInt4(void){
 	//Interrupcion ocacionada por la rotacion del encoder
 	Exti4Flag =1;
-	data = GPIO_ReadPin(&handlerPuertoA1);
-	clk = GPIO_ReadPin(&handlerPuertoA4);
+	data = GPIO_ReadPin(&handlerPuertoA1); //Lee el estado de DT
+	clk = GPIO_ReadPin(&handlerPuertoA4);  //Lee el estado del Clock
 
 }
 
