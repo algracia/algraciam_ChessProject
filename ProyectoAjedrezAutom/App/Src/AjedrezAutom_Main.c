88e2ba40ec65354d16f1688fe6ef4a54deff7159
algracia <agraciamartelo@gmail.com>
2023-05-10 18:25:18 -0500

    Creacion de una funcion Home
    Se probo el montaje para habilitar una funcion home en el sistema

--- ProyectoAjedrezAutom/App/Src/AjedrezAutom_Main.c
+++ ProyectoAjedrezAutom/App/Src/AjedrezAutom_Main.c
@@ -28,13 +28,13 @@ GPIO_Handler_t handlerSeñalM1				={0};
 GPIO_Handler_t handlerSeñalM2				={0};
 GPIO_Handler_t handlerDireccM1				={0};
 GPIO_Handler_t handlerDireccM2				={0};
+GPIO_Handler_t handlerEnableM1				={0};
+GPIO_Handler_t handlerEnableM2				={0};
 GPIO_Handler_t handlerSeñalServo			={0};
-GPIO_Handler_t handlerUserButton			={0};
+GPIO_Handler_t handlerEndStopX				={0};
+GPIO_Handler_t handlerEndStopY				={0};
 GPIO_Handler_t handlerPinTX					={0};
 GPIO_Handler_t handlerPinRX					={0};
-
-GPIO_Handler_t handlerPrueba1		={0};
-GPIO_Handler_t handlerPrueba2		={0};
 
 BasicTimer_Handler_t handlerTimerBlinky 	={0};
 
@@ -44,20 +44,30 @@ PWM_Handler_t handlerPwmServo				={0};
 
 USART_Handler_t handlerUSART2				={0};
 
-EXTI_Config_t handlerEXTIUserButton			={0};
+EXTI_Config_t handlerEXTIEndStopX				={0};
+EXTI_Config_t handlerEXTIEndStopY				={0};
 
 
 /*Variables*/
+
+//Las relacionadas al USART
 uint8_t USARTDataRecieved 		=0;
-char direccion[]				={0};
 char bufferMsg[64] 				={0};
-uint16_t pasosMotores 			=100;
-uint16_t pasosServo 			=35;
+
+//Las relacionadas a los pasos
+uint16_t pasosMotores 			=500;
+uint16_t pasosServo 			=40;
 uint16_t contadorPasosMotores	=0;
 uint16_t contadorPasosServo		=0;
 
+//Las banderas
+uint8_t endStopXFlag			=1;
+uint8_t endStopYFlag			=1;
+
 /*Headers de funciones*/
 void InitHardware (void);
+void Home(void);
+void BasicMove (void);
 
 int main(void) {
 
@@ -77,9 +87,15 @@ int main(void) {
 
 				sprintf(bufferMsg,"\nLa direccion es: %s", "Arriba");
 
+				//Activamos los enable de los drivers
+				GPIO_WritePin(&handlerEnableM1, 1);
+				GPIO_WritePin(&handlerEnableM2, 1);
+
+				//Ajustamos la direccion
 				GPIO_WritePin(&handlerDireccM1, 0); //CCW
 				GPIO_WritePin(&handlerDireccM2, 1); //CW
 
+				//Habilitamos las salidas de los PWM
 				enableOutput(&handlerPwmM1);
 				enableOutput(&handlerPwmM2);
 
@@ -92,9 +108,15 @@ int main(void) {
 
 				sprintf(bufferMsg,"\nLa direccion es: %s", "Abajo");
 
+				//Activamos los enable de los drivers
+				GPIO_WritePin(&handlerEnableM1, 1);
+				GPIO_WritePin(&handlerEnableM2, 1);
+
+				//Ajustamos la direccion
 				GPIO_WritePin(&handlerDireccM1, 1); //CW
 				GPIO_WritePin(&handlerDireccM2, 0); //CCW
 
+				//Habilitamos las salidas de los PWM
 				enableOutput(&handlerPwmM1);
 				enableOutput(&handlerPwmM2);
 
@@ -107,9 +129,15 @@ int main(void) {
 
 				sprintf(bufferMsg,"\nLa direccion es: %s", "Derecha");
 
+				//Activamos los enable de los drivers
+				GPIO_WritePin(&handlerEnableM1, 1);
+				GPIO_WritePin(&handlerEnableM2, 1);
+
+				//Ajustamos la direccion
 				GPIO_WritePin(&handlerDireccM1, 0); //CCW
 				GPIO_WritePin(&handlerDireccM2, 0); //CCW
 
+				//Habilitamos las salidas de los PWM
 				enableOutput(&handlerPwmM1);
 				enableOutput(&handlerPwmM2);
 
@@ -123,9 +151,15 @@ int main(void) {
 
 				sprintf(bufferMsg,"\nLa direccion es: %s", "Izquierda");
 
+				//Activamos los enable de los drivers
+				GPIO_WritePin(&handlerEnableM1, 1);
+				GPIO_WritePin(&handlerEnableM2, 1);
+
+				//Ajustamos la direccion
 				GPIO_WritePin(&handlerDireccM1, 1); //CW
 				GPIO_WritePin(&handlerDireccM2, 1); //CW
 
+				//Habilitamos las salidas de los PWM
 				enableOutput(&handlerPwmM1);
 				enableOutput(&handlerPwmM2);
 
@@ -138,8 +172,16 @@ int main(void) {
 
 				sprintf(bufferMsg,"\nLa direccion es: %s", "Diagonal derecha arriba");
 
+				//Activamos los enable de los drivers
+				GPIO_WritePin(&handlerEnableM1, 1);
+				GPIO_WritePin(&handlerEnableM2, 1);
+
+				//Ajustamos la direccion
 				GPIO_WritePin(&handlerDireccM1, 0); //CW
 
+				//Habilitamos las salida del PWM
+				//solo del motor que se mueve y
+				//deshabilitamos la del otro
 				enableOutput(&handlerPwmM1);
 				disableOutput(&handlerPwmM2);
 
@@ -152,8 +194,16 @@ int main(void) {
 
 				sprintf(bufferMsg,"\nLa direccion es: %s", "Diagonal izquierda arriba");
 
+				//Activamos los enable de los drivers
+				GPIO_WritePin(&handlerEnableM1, 1);
+				GPIO_WritePin(&handlerEnableM2, 1);
+
+				//Ajustamos la direccion
 				GPIO_WritePin(&handlerDireccM2, 1); //CCW
 
+				//Habilitamos las salida del PWM
+				//solo del motor que se mueve y
+				//deshabilitamos la del otro
 				disableOutput(&handlerPwmM1);
 				enableOutput(&handlerPwmM2);
 
@@ -166,8 +216,16 @@ int main(void) {
 
 				sprintf(bufferMsg,"\nLa direccion es: %s", "Diagonal derecha abajo");
 
+				//Activamos los enable de los drivers
+				GPIO_WritePin(&handlerEnableM1, 1);
+				GPIO_WritePin(&handlerEnableM2, 1);
+
+				//Ajustamos la direccion
 				GPIO_WritePin(&handlerDireccM1, 1); //CCW
 
+				//Habilitamos las salida del PWM
+				//solo del motor que se mueve y
+				//deshabilitamos la del otro
 				enableOutput(&handlerPwmM1);
 				disableOutput(&handlerPwmM2);
 
@@ -180,8 +238,16 @@ int main(void) {
 
 				sprintf(bufferMsg,"\nLa direccion es: %s", "Diagonal izquierda abajo");
 
+				//Activamos los enable de los drivers
+				GPIO_WritePin(&handlerEnableM1, 1);
+				GPIO_WritePin(&handlerEnableM2, 1);
+
+				//Ajustamos la direccion
 				GPIO_WritePin(&handlerDireccM2, 0); //CW
 
+				//Habilitamos las salida del PWM
+				//solo del motor que se mueve y
+				//deshabilitamos la del otro
 				disableOutput(&handlerPwmM1);
 				enableOutput(&handlerPwmM2);
 
@@ -194,6 +260,11 @@ int main(void) {
 
 				sprintf(bufferMsg,"\n%s", "STOP");
 
+				//Desactivamos los enable de los drivers
+				GPIO_WritePin(&handlerEnableM1, 0);
+				GPIO_WritePin(&handlerEnableM2, 0);
+
+				//Deshabilitamos todas las señales de PWM
 				disableOutput(&handlerPwmM1);
 				disableOutput(&handlerPwmM2);
 				disableOutput(&handlerPwmServo);
@@ -233,7 +304,7 @@ int main(void) {
 				break;
 			}
 
-			/*Ire cambiando el numero de pasos para el servo*/
+			/*Ire cambiando el numero de pasos para los motores*/
 			case 't':{
 				//Aumentamos el numero de pasos en 10
 				pasosMotores += 10;
@@ -252,7 +323,33 @@ int main(void) {
 				break;
 			}
 
+			/*Probamos la funcion Home*/
+			case 'H':{
+				sprintf(bufferMsg,"\nRegresando a Home");
+
+				Home();
+
+
+				break;
+			}
+
+			case 'h':{
+				sprintf(bufferMsg,"\nPosicionando en cero");
+
+				BasicMove();
+
+				break;
+			}
+
 			default:{
+
+				sprintf(bufferMsg,"\nIngrese un caracter valido para continuar");
+
+				//Desactivamos los enable de los drivers
+				GPIO_WritePin(&handlerEnableM1, 0);
+				GPIO_WritePin(&handlerEnableM2, 0);
+
+				//Deshabilitamos todas las salidas de PWM
 				disableOutput(&handlerPwmM1);
 				disableOutput(&handlerPwmM2);
 				disableOutput(&handlerPwmServo);
@@ -269,6 +366,11 @@ int main(void) {
 
 
 		if(contadorPasosMotores > pasosMotores){
+			//Desactivamos los enable de los drivers
+			GPIO_WritePin(&handlerEnableM1, 0);
+			GPIO_WritePin(&handlerEnableM2, 0);
+
+			//desactivamos las salidad de los PWM
 			disableOutput(&handlerPwmM1);
 			disableOutput(&handlerPwmM2);
 
@@ -299,35 +401,18 @@ void InitHardware (void){
 	GPIO_Config(&handlerOnBoardLed);
 	BasicTimer_Config(&handlerTimerBlinky);
 
-//	//Pruebas
-//	handlerPrueba1.pGPIOx 								= GPIOB;
-//	handlerPrueba1.GPIO_PinConfig.GPIO_PinNumber			= PIN_10;
-//	handlerPrueba1.GPIO_PinConfig.GPIO_PinMode			= GPIO_MODE_OUT;
-//	handlerPrueba1.GPIO_PinConfig.GPIO_PinOPType			= GPIO_OTYPE_PUSHPULL;
-//	handlerPrueba1.GPIO_PinConfig.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
-//	handlerPrueba1.GPIO_PinConfig.GPIO_PinSpeed			= GPIO_OSPEED_FAST;
-//
-//	handlerPrueba2.pGPIOx 								= GPIOA;
-//	handlerPrueba2.GPIO_PinConfig.GPIO_PinNumber			= PIN_8;
-//	handlerPrueba2.GPIO_PinConfig.GPIO_PinMode			= GPIO_MODE_OUT;
-//	handlerPrueba2.GPIO_PinConfig.GPIO_PinOPType			= GPIO_OTYPE_PUSHPULL;
-//	handlerPrueba2.GPIO_PinConfig.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
-//	handlerPrueba2.GPIO_PinConfig.GPIO_PinSpeed			= GPIO_OSPEED_FAST;
-//
-//	GPIO_Config(&handlerPrueba1);
-//	GPIO_Config(&handlerPrueba2);
 
 	/*Configuramos la Señal de los motores*/
-	handlerSeñalM1.pGPIOx 								= GPIOC;
-	handlerSeñalM1.GPIO_PinConfig.GPIO_PinNumber		= PIN_7;
+	handlerSeñalM1.pGPIOx 								= GPIOB;
+	handlerSeñalM1.GPIO_PinConfig.GPIO_PinNumber		= PIN_4;
 	handlerSeñalM1.GPIO_PinConfig.GPIO_PinMode			= GPIO_MODE_ALTFN;
 	handlerSeñalM1.GPIO_PinConfig.GPIO_PinOPType		= GPIO_OTYPE_PUSHPULL;
 	handlerSeñalM1.GPIO_PinConfig.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
 	handlerSeñalM1.GPIO_PinConfig.GPIO_PinSpeed			= GPIO_OSPEED_FAST;
 	handlerSeñalM1.GPIO_PinConfig.GPIO_PinAltFunMode	= AF2;
 
-	handlerSeñalM2.pGPIOx 								= GPIOC;
-	handlerSeñalM2.GPIO_PinConfig.GPIO_PinNumber		= PIN_6;
+	handlerSeñalM2.pGPIOx 								= GPIOA;
+	handlerSeñalM2.GPIO_PinConfig.GPIO_PinNumber		= PIN_7;
 	handlerSeñalM2.GPIO_PinConfig.GPIO_PinMode			= GPIO_MODE_ALTFN;
 	handlerSeñalM2.GPIO_PinConfig.GPIO_PinOPType		= GPIO_OTYPE_PUSHPULL;
 	handlerSeñalM2.GPIO_PinConfig.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
@@ -343,7 +428,7 @@ void InitHardware (void){
 
 	//PWM de los motores
 	handlerPwmM1.ptrTIMx				=TIM3;
-	handlerPwmM1.config.channel 		=PWM_CHANNEL_2;
+	handlerPwmM1.config.channel 		=PWM_CHANNEL_1;
 	handlerPwmM1.config.polarity 		=PWM_POLARITY_ACTIVE_LOW;
 	handlerPwmM1.config.prescaler 		=PWM_PRESCALER_100us;
 	handlerPwmM1.config.periodo 		=10; //Equivale a un periodo de 1ms
@@ -351,7 +436,7 @@ void InitHardware (void){
 	handlerPwmM1.config.interruption	=PWM_PERIOD_INTERRUPT_ENABLE;
 
 	handlerPwmM2.ptrTIMx				=TIM3;
-	handlerPwmM2.config.channel 		=PWM_CHANNEL_1;
+	handlerPwmM2.config.channel 		=PWM_CHANNEL_2;
 	handlerPwmM2.config.polarity 		=PWM_POLARITY_ACTIVE_LOW;
 	handlerPwmM2.config.prescaler 		=PWM_PRESCALER_100us;
 	handlerPwmM2.config.periodo 		=10; //Equivale a un periodo de 1ms
@@ -369,14 +454,14 @@ void InitHardware (void){
 
 	//Direccion motores
 	handlerDireccM1.pGPIOx 								= GPIOB;
-	handlerDireccM1.GPIO_PinConfig.GPIO_PinNumber		= PIN_2;
+	handlerDireccM1.GPIO_PinConfig.GPIO_PinNumber		= PIN_5;
 	handlerDireccM1.GPIO_PinConfig.GPIO_PinMode			= GPIO_MODE_OUT;
 	handlerDireccM1.GPIO_PinConfig.GPIO_PinOPType		= GPIO_OTYPE_PUSHPULL;
 	handlerDireccM1.GPIO_PinConfig.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
 	handlerDireccM1.GPIO_PinConfig.GPIO_PinSpeed		= GPIO_OSPEED_FAST;
 
-	handlerDireccM2.pGPIOx 								= GPIOC;
-	handlerDireccM2.GPIO_PinConfig.GPIO_PinNumber		= PIN_8;
+	handlerDireccM2.pGPIOx 								= GPIOA;
+	handlerDireccM2.GPIO_PinConfig.GPIO_PinNumber		= PIN_12;
 	handlerDireccM2.GPIO_PinConfig.GPIO_PinMode			= GPIO_MODE_OUT;
 	handlerDireccM2.GPIO_PinConfig.GPIO_PinOPType		= GPIO_OTYPE_PUSHPULL;
 	handlerDireccM2.GPIO_PinConfig.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
@@ -384,6 +469,28 @@ void InitHardware (void){
 
 	GPIO_Config(&handlerDireccM1);
 	GPIO_Config(&handlerDireccM2);
+
+	//Enable de los motores
+	handlerEnableM1.pGPIOx 								= GPIOC;
+	handlerEnableM1.GPIO_PinConfig.GPIO_PinNumber		= PIN_4;
+	handlerEnableM1.GPIO_PinConfig.GPIO_PinMode			= GPIO_MODE_OUT;
+	handlerEnableM1.GPIO_PinConfig.GPIO_PinOPType		= GPIO_OTYPE_PUSHPULL;
+	handlerEnableM1.GPIO_PinConfig.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
+	handlerEnableM1.GPIO_PinConfig.GPIO_PinSpeed		= GPIO_OSPEED_FAST;
+
+	handlerEnableM2.pGPIOx 								= GPIOA;
+	handlerEnableM2.GPIO_PinConfig.GPIO_PinNumber		= PIN_8;
+	handlerEnableM2.GPIO_PinConfig.GPIO_PinMode			= GPIO_MODE_OUT;
+	handlerEnableM2.GPIO_PinConfig.GPIO_PinOPType		= GPIO_OTYPE_PUSHPULL;
+	handlerEnableM2.GPIO_PinConfig.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
+	handlerEnableM2.GPIO_PinConfig.GPIO_PinSpeed		= GPIO_OSPEED_FAST;
+
+	GPIO_Config(&handlerEnableM1);
+	GPIO_Config(&handlerEnableM2);
+
+	//Iniciamos la señal a un valor conocido
+	GPIO_WritePin(&handlerEnableM1, 0); //un cero aqui, deshabilita los drivers
+	GPIO_WritePin(&handlerEnableM2, 0);
 
 
 	/*Configuramos la señal del Servomotor*/
@@ -440,11 +547,126 @@ void InitHardware (void){
 	handlerUSART2.USART_Config.USART_enableIntTX 	= USART_TX_INTERRUP_DISABLE;
 
 	USART_Config(&handlerUSART2);
+
+
+	/*Configuramos los EXTI para los finales de carrera*/
+
+	//Configuramos los pines como entradas
+	handlerEndStopX.pGPIOx								= GPIOA;
+	handlerEndStopX.GPIO_PinConfig.GPIO_PinNumber		= PIN_1;
+	handlerEndStopX.GPIO_PinConfig.GPIO_PinMode			= GPIO_MODE_IN;
+	handlerEndStopX.GPIO_PinConfig.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
+
+	handlerEndStopY.pGPIOx								= GPIOA;
+	handlerEndStopY.GPIO_PinConfig.GPIO_PinNumber		= PIN_4;
+	handlerEndStopY.GPIO_PinConfig.GPIO_PinMode			= GPIO_MODE_IN;
+	handlerEndStopY.GPIO_PinConfig.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
+
+	//Configuramos los handler del EXTI
+	handlerEXTIEndStopX.pGPIOHandler	=&handlerEndStopX;
+	handlerEXTIEndStopX.edgeType		=EXTERNAL_INTERRUPT_RISING_EDGE;
+
+	handlerEXTIEndStopY.pGPIOHandler	=&handlerEndStopY;
+	handlerEXTIEndStopY.edgeType		=EXTERNAL_INTERRUPT_RISING_EDGE;
+
+	//Cargamos las configuraciones
+	ExtInt_Config(&handlerEXTIEndStopX);
+	ExtInt_Config(&handlerEXTIEndStopY);
+}
+
+void Home(void){
+
+	/*Primero paramos todo por seguridad*/
+	disableOutput(&handlerPwmM1);
+	disableOutput(&handlerPwmM2);
+
+	/*Activamos los enable de los drivers*/
+	GPIO_WritePin(&handlerEnableM1, 1);
+	GPIO_WritePin(&handlerEnableM2, 1);
+
+	/*Bajamos la bandera en X para
+	que inicie el movimiento en esta direccion.*/
+	endStopXFlag =0;
+
+	/*Hacemos que se mueva a la izquierda*/
+	GPIO_WritePin(&handlerDireccM1, 1); //CW
+	GPIO_WritePin(&handlerDireccM2, 1); //CW
+
+	enableOutput(&handlerPwmM1);
+	enableOutput(&handlerPwmM2);
+
+	//Mientras no cambie la bandera, el se mantiene en un bucle
+	while(!endStopXFlag){
+		__NOP();
+	}
+
+	/*Desactivamos el movimiento en X*/
+	disableOutput(&handlerPwmM1);
+	disableOutput(&handlerPwmM2);
+
+
+	/*Ahora, bajamos la bandera en Y para
+	que inicie el movimiento en esta direccion.*/
+	endStopYFlag =0;
+
+	/*Hacemos que se mueva hacia abajo*/
+	GPIO_WritePin(&handlerDireccM1, 1); //CW
+	GPIO_WritePin(&handlerDireccM2, 0); //CCW
+
+	enableOutput(&handlerPwmM1);
+	enableOutput(&handlerPwmM2);
+
+	//Mientras no cambie la bandera, el se mantiene en un bucle
+	while(!endStopYFlag){
+		__NOP();
+	}
+
+	/*Desactivamos el movimiento en Y*/
+	disableOutput(&handlerPwmM1);
+	disableOutput(&handlerPwmM2);
+
+	/*desactivamos los enable de los drivers*/
+	GPIO_WritePin(&handlerEnableM1, 0);
+	GPIO_WritePin(&handlerEnableM2, 0);
+
+
+}
+
+void BasicMove (void) {
+
+	//Ahora, hacemos que se mueva hacia arriba
+	//y a partir de esta posicion, va a estar nuestro cero
+
+	/*Activamos los enable de los drivers*/
+	GPIO_WritePin(&handlerEnableM1, 1);
+	GPIO_WritePin(&handlerEnableM2, 1);
+
+	//Ajustamos la direccion
+	GPIO_WritePin(&handlerDireccM1, 0); //CCW
+	GPIO_WritePin(&handlerDireccM2, 1); //CW
+
+	//Habilitamos las salidas de los PWM
+	enableOutput(&handlerPwmM1);
+	enableOutput(&handlerPwmM2);
+
+	//Inicializamos el contador de pasos
+	contadorPasosMotores =0;
+
+	while(!(contadorPasosMotores > 385)){
+		__NOP();
+	}
+
+	//Detenmos todo
+	disableOutput(&handlerPwmM1);
+	disableOutput(&handlerPwmM2);
+
+//	/*desactivamos los enable de los drivers*/
+//	GPIO_WritePin(&handlerEnableM1, 1);
+//	GPIO_WritePin(&handlerEnableM2, 1);
 }
 
 
-
-/*Callbacks Timers*/
+/*Callbacks*/
 void BasicTimer2_Callback(void){
 	GPIOxTooglePin(&handlerOnBoardLed);
 
@@ -463,6 +685,14 @@ void BasicTimer4_Callback(void){
 void usart2Rx_Callback(void){
 	//Cada que se lanze la interrupcion, recibimos datos
 	USARTDataRecieved =getRxData();
+}
+
+void callback_extInt1(void){
+	endStopXFlag =1;
+}
+
+void callback_extInt4(void){
+	endStopYFlag =1;
 }
 
 
